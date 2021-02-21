3e01d5f25944928978d82624cab5d637e096507e
Marco Ortolani <m.ortolani@keele.ac.uk>
2021-02-21 21:23:31 +0000

    A comment after hello

--- sample/src/main/java/uk/ac/keele/csc20004/Main.java
+++ sample/src/main/java/uk/ac/keele/csc20004/Main.java
@@ -12,5 +12,6 @@ public class Main {
     public static void main(String[] args) {
       System.out.println("Initial message");
       System.out.println("Hello, world!");
+      System.out.println("Final message");
     }
 }
