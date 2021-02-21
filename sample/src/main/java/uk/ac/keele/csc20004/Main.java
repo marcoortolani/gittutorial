8b471ec2812504767b92a6eb45f5b1f3f2449eb2
marcoortolani <marco.ortolani@gmail.com>
2021-02-21 22:18:11 +0000

    gitpod accepts

--- sample/src/main/java/uk/ac/keele/csc20004/Main.java
+++ sample/src/main/java/uk/ac/keele/csc20004/Main.java
@@ -12,10 +12,13 @@ public class Main {
     public static void main(String[] args) {
       System.out.println("message 1");
       System.out.println("Hello, world!");
+<<<<<<< HEAD
       System.out.println("message");
       System.out.println("Hello, world!");
       System.out.println("Hello, world!");
       System.out.println("Hello, world!");
+=======
+>>>>>>> 77ea36d (gitpod changes)
       System.out.println("message 2");
     }
 }
