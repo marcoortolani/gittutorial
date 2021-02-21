f2c1de1fe5c433b7d86ca93a641ea955110503a1
Marco Ortolani <m.ortolani@keele.ac.uk>
2021-02-21 21:41:01 +0000

    another hello message

--- sample/src/main/java/uk/ac/keele/csc20004/Main.java
+++ sample/src/main/java/uk/ac/keele/csc20004/Main.java
@@ -12,6 +12,7 @@ public class Main {
     public static void main(String[] args) {
       System.out.println("Initial message");
       System.out.println("Hello, world!");
+      System.out.println("Hello, world!");
       System.out.println("Final message");
     }
 }
