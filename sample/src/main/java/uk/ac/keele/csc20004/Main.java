0def8408d023584209a22e24a3459d5467cfd1f0
marcoortolani <marco.ortolani@gmail.com>
2021-02-21 22:22:20 +0000

    ok

--- sample/src/main/java/uk/ac/keele/csc20004/Main.java
+++ sample/src/main/java/uk/ac/keele/csc20004/Main.java
@@ -13,6 +13,7 @@ public class Main {
       System.out.println("message 1");
       System.out.println("Hello, world!");
       System.out.println("message 2");
+      System.out.println("message gitpod");
     }
 }
  
