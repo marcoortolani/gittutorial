129d8e6d231943faa8c35107ad72ecb54744ef3d
Marco Ortolani <m.ortolani@keele.ac.uk>
2021-02-22 07:45:11 +0000

    netbeans tries to push change

--- sample/src/main/java/uk/ac/keele/csc20004/Main.java
+++ sample/src/main/java/uk/ac/keele/csc20004/Main.java
@@ -10,8 +10,9 @@ package uk.ac.keele.csc20004;
  */
 public class Main {
     public static void main(String[] args) {
-      System.out.println("Hello, world!");
-      System.out.println("Hello, world!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello, world!");
+        }
     }
 }
  
